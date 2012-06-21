e6773a77394550c6b9c18c2ad0ecabd1e730c9df
ibby <ibby.j@hotmail.com>
2012-06-20 20:10:42 -0400

    Trying to remove files and comments

--- newTestFiles/splitter.py
+++ newTestFiles/splitter.py
@@ -2,25 +2,27 @@
 
 import sys
 import fileinput
+#File names to be added, can be changed
 song_file, users_file, triplet_file = sys.argv[1:4]
 
 counter=1;
 
 songs={};
 users={};
+#Reading the song file and adding the song names to a hashmap
 for line in fileinput.input([song_file]):
 	songs[line.rstrip('\n').split(' ')[0]] = counter;
 	counter=counter+1;
 
 
 counter=1;
-
+#Reading the user file and adding the user names to a hashmap
 for line in fileinput.input([users_file]):
 	users[line.rstrip('\n').split(' ')[0]] = counter;
 	counter=counter+1;
 
 newfile = open("final_triplet.txt","w");
-
+#Reading the triplet file containing each name with songs the person listens to, converting this into an easier readable format
 for line in fileinput.input([triplet_file]):
 	temp = line.rstrip('\n').split('\t');
 	newfile.write(str(users[temp[0]])+" "+str(songs[temp[1]])+" "+temp[2]+'\n');	
