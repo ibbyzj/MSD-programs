81f2902c394f513123a205dc59cb5b3a65b3fa0c
ibby <ijumkh01@students.poly.edu>
2012-06-27 16:13:36 -0400

    Adding the final user writer file

--- newTestFiles/final_writer.py
+++ newTestFiles/final_writer.py
@@ -7,10 +7,15 @@ import fileinput
 import sys
 import numpy
 
+#test_output => Matrix file
+#test_triplet => Triplet file
+#out_file => output file with 500 song integers per line
 test_output, test_triplet, output_file = sys.argv[1:4]
 
+#This is a python function that reads the triplet file, and returns a list of songs per user
 def song_function(test_triplet):
 	old_user = 1;
+	#Lis to hold songs for user_id
 	listy = [];
 	for line in open(test_triplet):
 		try:
@@ -19,6 +24,7 @@ def song_function(test_triplet):
 			count = int(line.rstrip('\n').split(' ')[2]);
 		except ValueError:
 			continue
+		#If the user_id is different than the previous then empty the list and return it
 		if old_user != user_id:
 			yield listy;
 			listy = [];
@@ -29,31 +35,42 @@ def song_function(test_triplet):
 
 
 
+#Read the colisten matrix
 colisten = scipy.io.mmread(file(test_output)).tocsr();
 print("done reading matrix");
+
 diag = colisten.diagonal();
+diagonal_sorted = numpy.argsort(-diag)[:500];
 
-listenranked = numpy.argsort(-diag)[:500]
+#popular_file = open("popular_songs.txt","w");
+#for s in diagonal_sorted:
+	#popular_file.write(str(s+1));
+newFile = open(output_file,'w');
 
-with open(output_file, 'w') as out:
-	for song_count in song_function(test_triplet):
-		songs, count = zip(*song_count)
-		sim = numpy.array(count)[numpy.newaxis, :] * colisten[numpy.array(songs) - 1,:]
-		simidxs = sim.nonzero()[1]
-		srt = numpy.lexsort((-diag[simidxs], -sim[0,simidxs]))
-		rankidxs = simidxs[srt]
-    
-		guess = []
-		for s in rankidxs:
-			if s+1 in songs:
+#Each loop takes the song list for one user
+for song_count in song_function(test_triplet):
+	songs, count = zip(*song_count)
+	index=0;
+	for every_song in songs:
+		colistenArray = colisten[(songs[i]-1),:];
+		index+=1;
+		number_array = colistenArray.toarray();
+		required = number_array.nonzero()[0];
+		final_array = numpy.argsort(-(number_array))[0];
+		song_list = [];
+		for k in range(len(required)):
+			if c[k]+1 in songs:
 				continue
-			guess.append(str(s+1))
-			if len(guess) == 500: break
-			else:
-				for s in listenranked:
-					if s+1 in songs or s+1 in rankidxs:
-						continue
-				guess.append(str(s+1))
-			if len(guess) == 500: break
-	print("adding now....");
-	out.write(' '.join(guess) + '\n')
+			song_list.append(str(c[k]+1))
+			if len(song_list)==500:
+				break
+	
+	#Now check for the second list for incomplete songs			
+	for each_song in diagonal_sorted:
+		if str(each_song+1) in songs or str(each_song+1) in song_list:
+			continue
+		if len(song_list)==500:
+			break
+		song_list.append(str(song+1));
+	newFile.write(' '.join(song_list)+'\n');
+	
